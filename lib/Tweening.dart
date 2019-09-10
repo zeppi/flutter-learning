93855217b47d21357397c08ed8a24354b499b0d2
gdamiani <giuslee@gmail.com>
2019-09-10 21:33:38 +0200

    step-one-tweening, step 1.2

--- lib/Tweening.dart
+++ lib/Tweening.dart
@@ -8,6 +8,7 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 class Tweening extends StatefulWidget
 {
@@ -15,10 +16,60 @@ class Tweening extends StatefulWidget
   _TweeningState createState() => new _TweeningState();
 }
 
-class _TweeningState extends State<Tweening>
+class _TweeningState extends State<Tweening> with SingleTickerProviderStateMixin
 {
+  AnimationController _controller;
+  Animation<Color> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = new AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this
+    );
+  
+    animation = new ColorTween(
+      begin: const Color.fromRGBO(50, 10, 10, 0.5),
+      end: const Color.fromRGBO(0, 200, 100, 0.5)
+    ).animate(_controller);
+
+    animation.addListener(() {
+        setState((){});
+      });
+
+    animation.addStatusListener((s){
+      if(s == AnimationStatus.completed){
+        _controller.reverse();
+      } else if (s == AnimationStatus.dismissed){
+        _controller.forward();
+      }
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return new Container(child: new MyAnimation(animation: animation));
+  }
+}
+
+class MyAnimation extends AnimatedWidget {
+  const MyAnimation({Key key, Animation animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation animation = listenable;
+
     return new Container(
       child: new Center(
         child: new Row(
@@ -27,7 +78,27 @@ class _TweeningState extends State<Tweening>
             new Bar(),
             new Bar(),
             new Bar(),
-            new Bar(),
+        new Container(
+          width: 35.0,
+          height: 15.0,
+          decoration: new BoxDecoration(
+              color: const Color.fromRGBO(0, 0, 255, 1.0),
+              borderRadius: new BorderRadius.circular(10.0),
+              boxShadow: [
+                new BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 8.0,
+                  spreadRadius: 1.0,
+                  offset: new Offset(1.0, 0.0),
+                ),
+                new BoxShadow(
+                  color: animation.value,
+                  blurRadius: 6.0,
+                  spreadRadius: 1.5,
+                  offset: new Offset(1.0, 0.0),
+                ),
+              ]),
+        );,
           ],
         ),
       ),
@@ -36,6 +107,7 @@ class _TweeningState extends State<Tweening>
 }
 
 class Bar extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new Container(
